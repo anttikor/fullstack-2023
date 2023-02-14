a9c667954c7b42e301df189cc1cc1ee822598ec0
anttikor <a.kortelainen@gmail.com>
2023-02-14 12:38:22 +0200

    osa 1_1

---
+++ osa1/kurssitiedot/src/App.js
@@ -0,0 +1,52 @@
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <p>{props.name} - {props.points} points</p>
+
+    </div>
+  )
+}
+
+const Total = (props) => {
+  const total = props.points_total.reduce((acc, curr) => {
+    return acc + curr;
+  }, 0);
+
+  return (
+    <div>
+      <p>Total points: {total}</p>
+    </div>
+  )
+}
+
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  return (
+    <div>
+      <Header course = {course} />
+      <Content name = {part1} points = {exercises1}/>
+      <Content name = {part2} points = {exercises2}/>
+      <Content name = {part3} points = {exercises3}/>      
+      <Total points_total = {[exercises1,exercises2, exercises3]}/>  
+    </div>
+  )
+}
+
+
+export default App
